e764b6c503e533af859a5fb4cc3d0103c5a9968f
Anthony Scopatz <scopatz@gmail.com>
2017-11-09 23:16:07 -0500

    windows cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -ex
 
 if [[ $(uname) == "Linux" ]]; then
   # this changes the install dir from ${PREFIX}/lib64 to ${PREFIX}/lib
@@ -6,18 +7,16 @@ if [[ $(uname) == "Linux" ]]; then
   sed -i 's:@toolexeclibdir@:${libdir}:g' libffi.pc.in
 fi
 if [[ $(uname -o) == "Msys" ]]; then
-  ./configure --prefix="${PREFIX}" --includedir="${PREFIX}/include" \
-              --disable-debug --disable-dependency-tracking \
-              CC="${PWD}/msvcc.sh -m${ARCH}" \
-              CXX="${PWD}/msvcc.sh -m${ARCH}" \
-              LD=link \
-              CXXCPP="cl -nologo -EP" \
-              CPP="cl -nologo -EP" \
+  ./configure --prefix="${PREFIX}" \
+			  --includedir="${PREFIX}/include" \
+              --disable-debug \
+			  --disable-dependency-tracking \
               --build=x86_64-w64-mingw32 \
-              --enable-static --disable-shared
+              --enable-static \
+			  --disable-shared
 else
   ./configure --disable-debug --disable-dependency-tracking \
-              --prefix="${PREFIX}" --includedir="${PREFIX}/include" \
+              --prefix="${PREFIX}" --includedir="${PREFIX}/include" 
 fi
 make
 make check
