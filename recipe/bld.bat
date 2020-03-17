bccef22c8eaca42e8a74adee3d4a5a743ffe9398
Ryan Volz <rvolz@mit.edu>
2020-03-17 18:33:28 -0400

    v3.3 through patch to add CMake build files.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,23 +1,21 @@
-:: Delegate to the Unix script. We need to translate the key path variables
-:: to be Unix-y rather than Windows-y, though.
+setlocal EnableDelayedExpansion
 
-copy "%RECIPE_DIR%\build.sh" .
+:: Make a build folder and change to it
+mkdir build
+cd build
 
-set MSYSTEM=MINGW%ARCH%
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
-
-set "saved_recipe_dir=%RECIPE_DIR%"
-FOR /F "delims=" %%i IN ('cygpath.exe -u -p "%PATH%"') DO set "PATH_OVERRIDE=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_M=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_U=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PYTHON%"') DO set "PYTHON=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%RECIPE_DIR%"') DO set "RECIPE_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%SP_DIR%"') DO set "SP_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%STDLIB_DIR%"') DO set "STDLIB_DIR=%%i"
-
-bash -lc "./build.sh"
+cmake -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -DVERSION:STRING="%PKG_VERSION%" ^
+    ..
 if errorlevel 1 exit 1
-exit 0
+
+:: build
+cmake --build . --config Release -- -j%CPU_COUNT%
+if errorlevel 1 exit 1
+
+:: install
+cmake --build . --target install --config Release
+if errorlevel 1 exit 1
